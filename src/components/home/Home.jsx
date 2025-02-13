3c59377ba968482fd8a52d8ed730febb4a4c26d6
Jay Prakash <jayp7808@gmail.com>
2025-02-13 20:13:57 +0530

    css change of home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './home.css';
-import pizza from '../../photos/pizza.jpg'
-import burger from '../../photos/burger.jpg'
-import sushi from '../../photos/sushi.jpg'
-import pasta from '../../photos/pasta.jpg'
-import desserts from '../../photos/desserts.jpg'
+import pizza from '../../photos/pizza.jpg';
+import burger from '../../photos/burger.jpg';
+import sushi from '../../photos/sushi.jpg';
+import pasta from '../../photos/pasta.jpg';
+import desserts from '../../photos/desserts.jpg';
 
 const categories = ['Pizza', 'Burgers', 'Sushi', 'Pasta', 'Desserts'];
 
@@ -12,7 +12,7 @@ const foods = [
   { name: 'Pepperoni Pizza', category: 'Pizza', img: pizza },
   { name: 'Cheeseburger', category: 'Burgers', img: burger },
   { name: 'Sushi Rolls', category: 'Sushi', img: sushi },
-  { name: 'Spaghetti Carbonara', category: 'Pasta', img: pasta },
+  { name: 'Pasta Carbonara', category: 'Pasta', img: pasta },
   { name: 'Chocolate Cake', category: 'Desserts', img: desserts },
 ];
 
@@ -20,7 +20,8 @@ const Home = () => {
   return (
     <div className="home">
       <header className="header">
-        <h1>Food Ordering App</h1>
+        <h1>Welcome to Foodie Heaven 🍴</h1>
+        <p>Delicious meals, just a click away!</p>
       </header>
       
       <div className="categories">
@@ -36,6 +37,7 @@ const Home = () => {
           <div className="food-item" key={food.name}>
             <img src={food.img} alt={food.name} className="food-img" />
             <h3>{food.name}</h3>
+            <button className="order-button">Order Now</button>
           </div>
         ))}
       </div>
